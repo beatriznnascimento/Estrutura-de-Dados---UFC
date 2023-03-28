411e736b2f445e1fa08847db7238b6edbeae35a0
Beatriz Nascimento <beatriznascimento@alu.ufc.br>
2023-03-28 08:35:27 -0300

    update

---
+++ LAB02/wordquebrou.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    char frase[100];
+    char form;
+    for(int i = 0; i < 100; i++){  
+        cin >> frase[i]; // recebe a frase
+        cin >> form;
+
+        if(frase[i] == "")
+    }
+
+}
